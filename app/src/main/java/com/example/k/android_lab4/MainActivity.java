c39f8f02600536e206cac215ca27b0ea4ec0e4c8
Krzysztof Palczewski <kpalcze@gmail.com>
2017-06-03 16:37:07 +0200

    simple cursor adapter

--- app/src/main/java/com/example/k/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/k/android_lab4/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.k.android_lab4;
 
+import android.content.Intent;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private SimpleCursorAdapter adapter;
+    private MySQLite db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TODO asdasdasd
+        String[] values = new String[] {"jeden", "dwa", "trzy", "cztery", "piec"};
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+        db = new MySQLite(this);
+        this.adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                db.lista(),
+                new String[] {"_id", "gatunek"},
+                new int[] {android.R.id.text1, android.R.id.text2},
+                SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE);
+        ListView listview = (ListView) findViewById(
+                R.id.listView );
+        listview.setAdapter(this.adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi) {
+        Intent intencja = new Intent(this,
+                DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==1 && resultCode==RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            String nowy = (String)extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
