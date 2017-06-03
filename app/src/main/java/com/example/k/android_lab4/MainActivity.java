5f98833d4aff442d74c33fae727c9e7380bc19f7
Krzysztof Palczewski <kpalcze@gmail.com>
2017-06-03 16:53:50 +0200

    adding to sql db

--- app/src/main/java/com/example/k/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/k/android_lab4/MainActivity.java
@@ -58,8 +58,9 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode==1 && resultCode==RESULT_OK)
         {
             Bundle extras = data.getExtras();
-            String nowy = (String)extras.get("wpis");
-            target.add(nowy);
+            Animal nowy = (Animal) extras.getSerializable("nowy");
+            this.db.dodaj(nowy);
+            adapter.changeCursor(db.lista());
             adapter.notifyDataSetChanged();
         }
     }
